2decee37da6df63ae4ec93bb708188ad839ed6f9
SantishaR <82038785+SantishaR@users.noreply.github.com>
2021-06-17 18:29:54 +0200

    Changed function

--- function.js
+++ function.js
@@ -1,10 +1,9 @@
 function check()
 {
   let x;
-  x = prompt("Enter any number");
   if (x % 2 == 0){
-    console.log("even number");
+    alert("even number");
      } else {
-      console.log("odd number");
+      alert("odd number");
     }
 }
